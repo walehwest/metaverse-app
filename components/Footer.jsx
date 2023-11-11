0121347c9d29dfe97f16742a845ddb6de9643e09
Olawale A <102753160+walehwest@users.noreply.github.com>
2023-11-11 23:19:07 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => (
             METAVERUS
           </h4>
           <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+            Copyright © 2023 WalehWest. All rights reserved.
           </p>
 
           <div className="flex gap-4">
